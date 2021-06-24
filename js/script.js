d0d074c8ed76718d6243905c815b9b00b19bfda1
Bianca Cristina <email@gmail.com>
2021-06-24 18:04:36 -0300

    realizado curso da programaria

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("botarEnviar").addEventListener("click", validaformulario)
+
+function validaFormulario(){
+if(document.getElementById("nome").value != "" &&
+     document.getElementById("email").value != "" &&
+     document.getElementById("telefone").value != "" ){
+
+    alert("Prontinho! você receberá as novidades por email.")
+  }else{
+    alert("Por favor, preencha os campos nome e email.")
+  }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+Foi realizado dentro do IDE online https://replit.com/
+
+
+Iniciado e concluido o curso.
